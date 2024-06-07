48c6c99d06505db78dd042e46b806709a8c12634
shaikh sufyan <shaikhsufyan1811@gmail.com>
2024-06-07 15:39:02 +0530

    upd

--- src/database/db.js
+++ src/database/db.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost:27017/user")
+mongoose.connect("mongodb+srv://codewithsk99:UqYxThUqUSwVovN7@cluster0.ywhxpk0.mongodb.net/user")
  
 .then(()=>{
     console.log("DB Connect Succesfully");
